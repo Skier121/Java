04a263159ac85f9530f17c80b4e6dfac6263fe06
Baranov Egor <skier@tut.by>
2018-10-21 13:37:37 +0300

    Update pr-1-1/Main.java

---
+++ pr-1-1/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=5;
+        if(a==b&b==c){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
