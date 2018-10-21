370d6a799c82e6841da67a4e047401158e710e57
Baranov Egor <skier@tut.by>
2018-10-21 20:39:59 +0300

    s

---
+++ pr1-7/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=96431;
+        int b=3653;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+" "+b);
+    }
+}
