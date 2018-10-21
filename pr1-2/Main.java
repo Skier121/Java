da89c9ed378c82e137651fb12dbcb75c1ca13ed3
Baranov Egor <skier@tut.by>
2018-10-21 15:45:36 +0300

    Update pr1-2/Main.java

---
+++ pr1-2/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mass=1356;
+        System.out.println(mass*1000+" gr");
+        System.out.println(mass*1000000+" mgr");
+        System.out.println(mass/1000+" t");
+    }
+}
