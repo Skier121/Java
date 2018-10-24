7806175a49afb2ea0aa6d17ec3150d1ee033830c
Baranov Egor <skier@tut.by>
2018-10-24 19:55:44 +0300

    Update pr1-4/Main.java

--- pr1-4/Main.java
+++ pr1-4/Main.java
@@ -3,28 +3,24 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String number="9643";
-        char[] arr=number.toCharArray();
-        boolean res1=true;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>arr[i-1]){
-                res1=false;
-                break;
-            }
-        }
-        boolean res2=true;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[i-1]){
-                res2=false;
-                break;
-            }
-        }
-        if(res1||res2){
-            System.out.println("stairway");
-        }
-        else{
-            System.out.println("no stairway");
-        }
-    }
+	public static void main(String[] args) {
+		//переделал на занятии с for на while
+		String number="9643";
+		char[] arr=number.toCharArray();
+		int i=1;
+		while(arr[i]>arr[i-1]&i<4){
+			i++;
+		}
+		int j=1;
+		while(arr[i]<arr[i-1]&j<4){
+			j++;
+		}
+		if(i==4||j==4){
+			System.out.println("stairway");
+		}
+		else{
+			System.out.println("no stairway");
+		}
+	}
 }
+
